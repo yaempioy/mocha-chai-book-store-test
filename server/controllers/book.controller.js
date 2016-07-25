bae557ea818750e9eedd0e4dbfbd6d7861781527
Kitti Boonchan <kornkittig@gmail.com>
2016-07-26 02:06:51 +0700

    Usage saveAsync which is the promisified version of model object for bluebird dependence

--- server/controllers/book.controller.js
+++ server/controllers/book.controller.js
@@ -14,17 +14,12 @@ function getBooks(req, res) {
 /**
  * POST: /api/book to save a new book.
  */
-function postBook(req, res) {
+function postBook(req, res, next) {
   let newBook = new Book(req.body)
   //Save it into the DB.
-  newBook.save((err,book) => {
-     if(err) {
-         res.send(err)
-     }
-     else { //If no errors, send it back to the client
-         res.json({message: "Book successfully added!", book })
-     }
-  })
+  newBook.saveAsync()
+    .then((saveBook) =>  res.json(saveBook))
+    .error((e) => next(e))
 }
 
 export default { getBooks, postBook }
