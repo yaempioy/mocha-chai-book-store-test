2783043819e6921506cc82d18740565eec1fb495
Kitti Boonchan <kornkittig@gmail.com>
2016-07-26 02:14:05 +0700

    Test create new book api

--- test/book.test.js
+++ test/book.test.js
@@ -7,7 +7,15 @@ import app from '../index'
 
 chai.config.includeStack = true;
 
-describe('## Book', () => {
+describe('## Book APIs', () => {
+
+  let book = {
+    title: 'Title Test',
+    author: 'Mr. Test',
+    year: 2016,
+    pages: 999
+  }
+
   describe('# GET /api/', () => {
     it('should return OK', (done) => {
       request(app)
@@ -18,5 +26,21 @@ describe('## Book', () => {
           done();
         });
     });
+  });
+
+  describe('# POST /api/book', () => {
+    it('should create a new book', (done) => {
+      request(app)
+        .post('/api/book')
+        .send(book)
+        .expect(httpStatus.OK)
+        .then(res => {
+          expect(res.body.title).to.equal(book.title)
+          expect(res.body.author).to.equal(book.author)
+          expect(res.body.year).to.equal(book.year)
+          expect(res.body.pages).to.equal(book.pages)
+          done()
+        })
+    })
   });
 });
